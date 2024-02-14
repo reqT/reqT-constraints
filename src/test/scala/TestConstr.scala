7b0b100efe070173a033f29ecb20525982e215a0
bjornregnell <bjorn.regnell@cs.lth.se>
2024-02-14 19:04:33 +0100

    remove solver from test cases

--- src/test/scala/TestConstr.scala
+++ src/test/scala/TestConstr.scala
@@ -1,12 +1,10 @@
 package reqt
 
 import constraints.*
-import solver.*
-import jacop.*
 
 class TestConstr extends munit.FunSuite:
 
-  test("Single-variable relations, using constraints factory"):
+  test("IntVar relation constraints"):
     val x = IntVar(id = "x")
     val cs = Seq(
       x === 42,
@@ -23,32 +21,9 @@ class TestConstr extends munit.FunSuite:
       XltC(x,43),
     ))
 
-  test("simple solve"):
-    val x = IntVar(id = "x")
-    val cs = Seq(
-      x === 42,
-      x >= 42,
-      x <= 42,
-      x > 41,
-      x < 43,
-    )
-    val result = cs.satisfy
-    assert(result.single(x) == Some(42)) 
 
-  test("EnumVar using String"):
+  test("EnumVar String value relation constraints"):
     val x = EnumVar("Color", Seq("Red", "Black","White"))
     val cs = Seq(x === x.toInt("Black"))
-    val result: String = cs.satisfy.last(x).get
-    assert(result ==  "Black")
+    assert(cs == Seq(XeqC(x, x.toInt("Black"))))
 
-  test("EnumVar using enum"):
-    enum Color { case Red, Black, White}
-    val x = EnumVar(Color, Color.values)
-    val cs = Seq(x === x.toInt(Color.Black))
-    val result: Color = cs.satisfy.last(x).get
-    assert(result ==  Color.Black)
-
-  test("EnumVar is bound by values"):
-    val x = EnumVar("Col", Seq("B","C"))
-    val result: Result = Seq(x === 42).satisfy
-    assert(result.conclusion == Conclusion.InconsistencyFound)
